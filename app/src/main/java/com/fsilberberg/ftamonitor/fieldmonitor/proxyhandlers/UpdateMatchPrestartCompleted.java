29e4cb80c2cd98ae9c8f820766d831d209315be1
Fredric Silberberg <fredric@fsilberberg.com>
2014-08-22 18:03:24 -0400

    Added new handler

---
+++ app/src/main/java/com/fsilberberg/ftamonitor/fieldmonitor/proxyhandlers/UpdateMatchPrestartCompleted.java
@@ -0,0 +1,23 @@
+package com.fsilberberg.ftamonitor.fieldmonitor.proxyhandlers;
+
+import com.google.gson.JsonObject;
+
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler1;
+
+import static com.fsilberberg.ftamonitor.common.MatchStatus.PRESTART_COMPLETED;
+
+/**
+ * Created by Fredric on 8/22/14.
+ */
+public class UpdateMatchPrestartCompleted extends ProxyHandlerBase implements SubscriptionHandler1<JsonObject> {
+
+    protected UpdateMatchPrestartCompleted(String proxyMethod) {
+        super(proxyMethod);
+    }
+
+    @Override
+    public void run(JsonObject object) {
+        updateMatchInfo(object);
+        m_fieldStatus.setMatchStatus(PRESTART_COMPLETED);
+    }
+}
