e82a7a10d67e8f69cdaa3c70debe2660c9ed7cd8
Fredric Silberberg <fredric@fsilberberg.com>
2014-08-25 12:08:55 -0400

    Settings updates on custom url being entered

--- app/src/main/java/com/fsilberberg/ftamonitor/FTAMonitorApplication.java
+++ app/src/main/java/com/fsilberberg/ftamonitor/FTAMonitorApplication.java
@@ -6,21 +6,27 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.fsilberberg.ftamonitor.fieldmonitor.FieldConnectionService;
+import com.fsilberberg.ftamonitor.fieldmonitor.SettingsUrlChangeListener;
 
 /**
  * Created by Fredric on 8/23/14.
  */
 public class FTAMonitorApplication extends Application {
 
-
     @Override
     public void onCreate() {
         super.onCreate();
 
+        // Start the FMS Service
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         String url = preferences.getString(getString(R.string.fms_ip_addr_key), "10.0.100.5");
         Intent serviceIntent = new Intent(getBaseContext(), FieldConnectionService.class);
         serviceIntent.putExtra(FieldConnectionService.URL_INTENT_EXTRA, url);
         startService(serviceIntent);
+
+        // Set up the settings change listener
+        String defaultKey = getString(R.string.on_field_key);
+        String urlKey = getString(R.string.fms_ip_addr_key);
+        preferences.registerOnSharedPreferenceChangeListener(new SettingsUrlChangeListener(defaultKey, urlKey, getBaseContext()));
     }
 }
