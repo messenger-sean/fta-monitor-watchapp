955351b85c0a6e02148d3d637494142fe1586034
Fredric Silberberg <fredric@fsilberberg.com>
2014-10-23 15:56:52 -0400

    Fully converted off of deprecated APIs

--- app/src/main/java/com/fsilberberg/ftamonitor/view/DrawerActivity.java
+++ app/src/main/java/com/fsilberberg/ftamonitor/view/DrawerActivity.java
@@ -1,13 +1,11 @@
 package com.fsilberberg.ftamonitor.view;
 
-import android.app.Activity;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -48,7 +46,7 @@ public class DrawerActivity extends ActionBarActivity {
 
         // Set up the drawer toggle and listeners
         m_drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        m_drawerToggle = new ActionBarDrawerToggle(this, m_drawerLayout, R.drawable.ic_drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        m_drawerToggle = new ActionBarDrawerToggle(this, m_drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
