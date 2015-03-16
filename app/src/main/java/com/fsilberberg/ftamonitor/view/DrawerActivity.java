441f173e3707460d256e9989f6f40c2f6446a69e
Fredric Silberberg <fredric@fsilberberg.com>
2015-03-15 23:12:55 -0400

    Added dynamic drawer creation based on whether or not the Field Monitor and Testing are enabled. This code is relatively fragile, and will likely break when I start adding more elements to the drawer.

--- app/src/main/java/com/fsilberberg/ftamonitor/view/DrawerActivity.java
+++ app/src/main/java/com/fsilberberg/ftamonitor/view/DrawerActivity.java
@@ -2,8 +2,10 @@ package com.fsilberberg.ftamonitor.view;
 
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,7 +19,14 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
+import java.util.ArrayList;
+
 public class DrawerActivity extends ActionBarActivity {
+
+    public static final String START_FRAGMENT = "start_fragment";
+    public static final int FIELD_MONITOR = 0;
+    public static final int TESTING = 1;
+    public static final int SETTINGS = 2;
 
     private Drawer.Result m_drawer = null;
     private boolean m_backButtonPressed = false;
@@ -25,24 +34,31 @@ public class DrawerActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
-        String[] drawerItems = getResources().getStringArray(R.array.drawer_tabs);
+        // Check to see which screens are enabled
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean fieldMonitor = prefs.getBoolean(getResources().getString(R.string.field_monitor_enabled_key), true);
+        final boolean testing = prefs.getBoolean(getResources().getString(R.string.testing_enabled_key), false);
 
         // Handle Toolbar
+        ArrayList<IDrawerItem> items = new ArrayList<>();
+        if (fieldMonitor) {
+            items.add(new PrimaryDrawerItem().withName(R.string.field_monitor_drawer));
+            // Only add testing if the field monitor is enabled in the first place
+            if (testing) {
+                items.add(new PrimaryDrawerItem().withName(R.string.settings_drawer));
+            }
+        }
+        items.add(new PrimaryDrawerItem().withName(R.string.settings_drawer));
+
         m_drawer = new Drawer()
                 .withActivity(this)
                 .withActionBarDrawerToggle(true)
                 .withTranslucentStatusBar(false)
                 .withActionBarDrawerToggleAnimated(true)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withName(drawerItems[0]),
-                        new PrimaryDrawerItem().withName(drawerItems[1]),
-                        new PrimaryDrawerItem().withName(drawerItems[2])
-                )
+                .withDrawerItems(items)
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
@@ -51,18 +67,32 @@ public class DrawerActivity extends ActionBarActivity {
                                 FragmentTransaction trans = getFragmentManager().beginTransaction();
                                 switch (position) {
                                     case 0:
-                                        trans.replace(
-                                                R.id.frame_container,
-                                                new FieldMonitorFragment(),
-                                                FieldMonitorFragment.class.getName()
-                                        );
+                                        if (!fieldMonitor) {
+                                            trans.replace(
+                                                    R.id.frame_container,
+                                                    new SettingsFragment(),
+                                                    SettingsFragment.class.getName());
+                                        } else {
+                                            trans.replace(
+                                                    R.id.frame_container,
+                                                    new FieldMonitorFragment(),
+                                                    FieldMonitorFragment.class.getName()
+                                            );
+                                        }
                                         break;
                                     case 1:
-                                        trans.replace(
-                                                R.id.frame_container,
-                                                new TestingFragment(),
-                                                TestingFragment.class.getName()
-                                        );
+                                        if (!testing) {
+                                            trans.replace(
+                                                    R.id.frame_container,
+                                                    new SettingsFragment(),
+                                                    SettingsFragment.class.getName());
+                                        } else {
+                                            trans.replace(
+                                                    R.id.frame_container,
+                                                    new TestingFragment(),
+                                                    TestingFragment.class.getName()
+                                            );
+                                        }
                                         break;
                                     case 2:
                                         trans.replace(
@@ -83,7 +113,45 @@ public class DrawerActivity extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        getFragmentManager().beginTransaction().replace(R.id.frame_container, new FieldMonitorFragment(), FieldMonitorFragment.class.getName()).commit();
+        // Check to see if we have a default screen, and if we do, switch to it
+        int startScreen = fieldMonitor ? FIELD_MONITOR : SETTINGS;
+        if (getIntent() != null && getIntent().hasExtra(START_FRAGMENT)) {
+            int extra = getIntent().getIntExtra(START_FRAGMENT, startScreen);
+            switch (extra) {
+                case FIELD_MONITOR:
+                    if (fieldMonitor) {
+                        startScreen = FIELD_MONITOR;
+                    }
+                    break;
+                case TESTING:
+                    if (fieldMonitor && testing) {
+                        startScreen = TESTING;
+                    }
+                    break;
+                case SETTINGS:
+                    startScreen = SETTINGS;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        FragmentTransaction trans = getFragmentManager().beginTransaction();
+        switch (startScreen) {
+            case FIELD_MONITOR:
+                trans.replace(R.id.frame_container, new FieldMonitorFragment(), FieldMonitorFragment.class.getName());
+                m_drawer.setSelection(0);
+                break;
+            case TESTING:
+                trans.replace(R.id.frame_container, new TestingFragment(), TestingFragment.class.getName());
+                m_drawer.setSelection(1);
+                break;
+            case SETTINGS:
+                trans.replace(R.id.frame_container, new SettingsFragment(), SettingsFragment.class.getName());
+                m_drawer.setSelection(items.size() - 1);
+                break;
+        }
+        trans.commit();
     }
 
     @Override
