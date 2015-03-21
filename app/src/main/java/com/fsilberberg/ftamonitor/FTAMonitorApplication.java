1f77a3c6519ee45075acc951d1751e3d1bedc34e
Fredric Silberberg <fredric@fsilberberg.com>
2015-03-21 13:23:43 -0400

    Updated the lifecycle of the FieldConnectionService. There is now a central ServicePreferenceListener that knows what keys are relevant to each service or to multiple services, and it is registered on application startup by FTAMonitorApplication. I

--- app/src/main/java/com/fsilberberg/ftamonitor/FTAMonitorApplication.java
+++ app/src/main/java/com/fsilberberg/ftamonitor/FTAMonitorApplication.java
@@ -5,9 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import com.fsilberberg.ftamonitor.fieldmonitor.FieldMonitorFactory;
-import com.fsilberberg.ftamonitor.services.FieldConnectionService;
-import com.fsilberberg.ftamonitor.services.FieldProblemNotificationService;
-import com.fsilberberg.ftamonitor.services.PebbleCommunicationService;
+import com.fsilberberg.ftamonitor.services.ServicePreferenceListener;
 
 /**
  * This is the main entry point of the application. On startup, it will create the main foreground
@@ -31,27 +29,10 @@ public class FTAMonitorApplication extends Application {
         // Initialize the field monitor factory
         FieldMonitorFactory.initialize();
 
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-        String fmEnabledKey = getString(R.string.field_monitor_enabled_key);
-        String pebbleEnabledKey = getString(R.string.pebble_key);
-        String notificationKey = getString(R.string.notification_key);
-        boolean fmEnabled = preferences.getBoolean(fmEnabledKey, true);
-
-        // If the field monitor is enabled, start the connection service
-        if (fmEnabled) {
-            FieldConnectionService.start();
-
-            // If the Pebble service is also enabled, start that
-            boolean pebbleEnabled = preferences.getBoolean(pebbleEnabledKey, false);
-            if (pebbleEnabled) {
-                PebbleCommunicationService.start();
-            }
-
-            // If the notification service is also enabled start that
-            boolean notificationEnabled = preferences.getBoolean(notificationKey, false);
-            if (notificationEnabled) {
-                FieldProblemNotificationService.start();
-            }
-        }
+        // Register the global shared preference listener and update all services
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(_context);
+        ServicePreferenceListener listener = new ServicePreferenceListener(_context);
+        prefs.registerOnSharedPreferenceChangeListener(listener);
+        listener.updateAllServices();
     }
 }
